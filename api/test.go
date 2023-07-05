7be643f241e9ce9ee179dab06cc0f8b1b2f867c5
muhammad arham <arhamymr@gmail.com>
2023-07-05 22:22:28 +0800

    update function

--- api/test.go
+++ api/test.go
@@ -3,8 +3,10 @@ package handler
 import (
 	"fmt"
 	"net/http"
+
+	"rsc.io/quote"
 )
  
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+  fmt.Fprintf(w, "<h1>Hello from Go!</h1>" + quote.Go())
 }
